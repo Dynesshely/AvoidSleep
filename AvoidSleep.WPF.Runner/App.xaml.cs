08597619cdaedeed682d39512033c71ba501ad10
Dynesshely <catrol@qq.com>
2023-05-19 21:56:16 +0800

    💾 📝 📦 Feat, Chore, Struct(AvoidSleep.WPF.Runner): Added runner project as container to debug easilier.

---
+++ AvoidSleep.WPF.Runner/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using KitX.Contract.CSharp;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Windows;
+
+namespace AvoidSleep.WPF.Runner;
+
+public partial class App : Application
+{
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        var dirPath = Path.GetFullPath(".");
+        var fileName = "AvoidSleep.WPF.dll";
+
+        var catalog = new DirectoryCatalog(dirPath, fileName);
+
+        var container = new CompositionContainer(catalog);
+
+        var sub = container.GetExportedValues<IIdentityInterface>();
+
+        foreach (var item in sub)
+        {
+            var controller = item.GetController();
+
+            controller.Start();
+
+            break;
+        }
+    }
+}
